0b1d59de1beaf4114e3dcf3cabe34df377feebb1
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-12-09 20:49:40 +0000

    Wrote Circle.cpp

---
+++ Circle.cpp
@@ -0,0 +1,18 @@
+#include "Circle.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Circle::Circle(double xCenter, double yCenter, double radius, string name) {
+    this->xCenter = xCenter;
+    this->yCenter = yCenter;
+    this->radius = radius;
+    setName(name);
+    calcArea();
+    
+}
+
+void Circle::calcArea() {
+    setArea(3.14159 * radius * radius);
+}
